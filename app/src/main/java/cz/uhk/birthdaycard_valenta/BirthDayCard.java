d8ecfa8f63738754a92f92c9525be062797bd1df
ee5415 <ee5415>
2016-07-14 11:06:04 +0800

    Initial commit

---
+++ app/src/main/java/cz/uhk/birthdaycard_valenta/BirthDayCard.java
@@ -0,0 +1,13 @@
+package cz.uhk.birthdaycard_valenta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BirthDayCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_birth_day_card);
+    }
+}
